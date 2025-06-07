1f6bf8c7028152af20d8886ea06d62093e23bdc3
Urvashi <urvashi8177singh@gmail.com>
2025-06-08 00:45:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+  const words = ["Tech Architect", "Developer"];
+  let i = 0;
+  let timer;
+  let isDeleting = false;
+  let txt = '';
+  const speed = 150;
+  const wait = 1000;
+  const element = document.getElementById("typewriter");
+
+  function type() {
+    const current = words[i % words.length];
+    if (isDeleting) {
+      txt = current.substring(0, txt.length - 1);
+    } else {
+      txt = current.substring(0, txt.length + 1);
+    }
+
+    element.innerHTML = txt;
+
+    if (!isDeleting && txt === current) {
+      setTimeout(() => {
+        isDeleting = true;
+        type();
+      }, wait);
+    } else if (isDeleting && txt === '') {
+      isDeleting = false;
+      i++;
+      type();
+    } else {
+      timer = setTimeout(type, isDeleting ? speed / 2 : speed);
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    type();
+  });
+  window.onscroll = function() {
+    var header = document.querySelector(".header");
+    
+    
+    if (window.scrollY > 27) { 
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+};
