ee0ac6599c678dfbc4bede15dabb2205a73f7c62
urvashi <urvashi8177singh@gmail.com>
2026-02-17 22:27:10 +0530

    final

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
+const words = [ "Tech Architect", "Developer" , "Designer"];
+let i = 0;
+let timer;
+let isDeleting = false;
+let txt = "";
+const speed = 150;
+const wait = 1000;
 
-  const words = ["Tech Architect", "Developer"];
-  let i = 0;
-  let timer;
-  let isDeleting = false;
-  let txt = '';
-  const speed = 150;
-  const wait = 1000;
-  const element = document.getElementById("typewriter");
-
-  
-
-  function type() {
-    const current = words[i % words.length];
-    if (isDeleting) {
-      txt = current.substring(0, txt.length - 1);
-    } else {
-      txt = current.substring(0, txt.length + 1);
-    }
-
-    element.innerHTML = txt;
-
-    if (!isDeleting && txt === current) {
-      setTimeout(() => {
-        isDeleting = true;
-        type();
-      }, wait);
-    } else if (isDeleting && txt === '') {
-      isDeleting = false;
-      i++;
-      type();
-    } else {
-      timer = setTimeout(type, isDeleting ? speed / 2 : speed);
-    }
+function type(element) {
+  const current = words[i % words.length];
+  if (isDeleting) {
+    txt = current.substring(0, txt.length - 1);
+  } else {
+    txt = current.substring(0, txt.length + 1);
   }
 
-  document.addEventListener("DOMContentLoaded", () => {
-    type();
-  });
-  window.onscroll = function() {
-    var header = document.querySelector(".header");
-    
-    
-    if (window.scrollY > 27) { 
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
+  element.innerHTML = txt;
+
+  if (!isDeleting && txt === current) {
+    setTimeout(() => {
+      isDeleting = true;
+      type(element);
+    }, wait);
+  } else if (isDeleting && txt === "") {
+    isDeleting = false;
+    i++;
+    type(element);
+  } else {
+    timer = setTimeout(() => type(element), isDeleting ? speed / 2 : speed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const element = document.getElementById("typewriter");
+  if (element) type(element);
+});
+
+window.onscroll = function () {
+  var header = document.querySelector(".header");
+
+  if (window.scrollY > 27) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
 };
